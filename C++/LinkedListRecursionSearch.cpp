441f64a919e06c44819aa8728423002707f29c3f
George <djordje_obradovic33@protonmail.com>
2022-03-11 13:49:16 +0200

    Update LinkedListRecursionSearch.cpp

--- C++/LinkedListRecursionSearch.cpp
+++ C++/LinkedListRecursionSearch.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : LinkedListRecursionSearch.cpp
-// Author      : DjordjeObradovic
+// Author      : 
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
